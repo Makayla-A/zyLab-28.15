05315b660cb379cc1f0e85f971997c2fe3610553
camerontofani <152201676+camerontofani@users.noreply.github.com>
2023-12-01 12:45:23 -0600

    Update main.cpp
    - fixed step 7

--- main.cpp
+++ main.cpp
@@ -12,41 +12,64 @@ void PrintMenu(const string playlistTitle) {
         << "s - Output songs by specific artist\n"
         << "t - Output total time of playlist (in seconds)\n"
         << "o - Output full playlist\n"
-        << "q - Quit";  
+        << "q - Quit" << endl;
 }
 PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
-   //STEP 4 (complete)
-   if (char == 'a'){
-    //ADD SONG (STEP 7) 
+   //STEP 4 
+   if (option == 'a'){   //ADD SONG  (STEP 7)
+      
     cout << "Enter song's unique ID: " << endl;
     cin >> uniqueID;
+    
     cout << "Enter song's name: " << endl;
-    cin >> songName;
+    cin.ignore();
+    getline(cin, songName);
+    
     cout << "Enter artist's name: " << endl;
-    cin >> artistName;
+    cin.ignore();
+    getline(cin, artistName);
+    
     cout << "Enter song's length (in seconds): " << endl;
     cin >> songLength;
-   }else if (char == 'd'){
-    //REMOVE SONG (STEP 8) 
-   }else if (char == 'c'){
-    //CHANGE POSITION OF SONG (STEP 9) 
-   } else if (char == 's'){
-    //OUTPUT SONGS BY SPESIFIC ARTIST (STEP 10)  
-   }else if (char == 't'){
-    //OUTPUT TOTAL TIME OF PLAYLIST (STEP 11)
-   }else if (char == 'o'){
-    //OUTPUT FULL PLAYLIST (STEP 6)
-   }else if (char == 'q'){
-    //QUIT (i think this should just be a break statement?)
+    
+    // create a new PlaylistNode with the entered info
+    
+   PlaylistNode* newNode = new PlaylistNode(uniqueID, songName, artistName, songLength);
+   
+   // If the list is empty, make the new node the head
+        if (headNode == nullptr) {
+            headNode = newNode;
+        } else {
+            // Traverse the list to find the last node
+            PlaylistNode* current = headNode;
+            while (current->GetNext() != nullptr) {  //while the next node is not empty
+                current = current->GetNext();   //go through all of the nodes until the last one 
+            } 
+            // Add the new node after the last node
+            current->SetNext(newNode);
+        }
+
+   }else if (option == 'd'){
+    //REMOVE SONG  
+   }else if (option == 'c'){
+    //CHANGE POSITION OF SONG  
+   } else if (option == 's'){
+    //OUTPUT SONGS BY SPESIFIC ARTIST   
+   }else if (option == 't'){
+    //OUTPUT TOTAL TILE OF PLAYLIST  
+   }else if (option == 'o'){
+    //OUTPUT FULL PLAYLIST 
+   }else if (option == 'q'){
+    //QUIT 
    }
    
-}//end of ExecuteMenu
+}
 
 int main() {
-   //STEP 2 (complete)
+   //STEP 2
    cout << " Enter playlist's title: :" << endl;
    cin >> playlistTitle;
    
-   return 0; 
+   return 0;
 }
 
