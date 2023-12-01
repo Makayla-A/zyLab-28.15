a0edc6f9ad3f02c7ca75f6b34792b88e1b08666a
camerontofani <152201676+camerontofani@users.noreply.github.com>
2023-12-01 11:39:01 -0600

    Update main.cpp
    steps 2, 3, and 4. 
    2: prompted user to set up menu 
    3: created menu output
    4: if/else statements for the choice the user makes

--- main.cpp
+++ main.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include "PlaylistNode.h"
+
 using namespace std;
 
-int main(){
-return 0;
+void PrintMenu(const string playlistTitle) {
+   //STEP 3
+   cout << playlistTitle << "PLAYLIST MENU" << endl;
+   cout << "a - Add song\n"
+        << "d - Remove song\n"
+        << "c - Change position of song\n"
+        << "s - Output songs by specific artist\n"
+        << "t - Output total time of playlist (in seconds)\n"
+        << "o - Output full playlist\n"
+        << "q - Quit";  
 }
+PlaylistNode* ExecuteMenu(char option, string playlistTitle, PlaylistNode* headNode) {
+   //STEP 4 (complete)
+   if (char == 'a'){
+    //ADD SONG (STEP 7)   
+   }else if (char == 'd'){
+    //REMOVE SONG (STEP 8) 
+   }else if (char == 'c'){
+    //CHANGE POSITION OF SONG (STEP 9) 
+   } else if (char == 's'){
+    //OUTPUT SONGS BY SPESIFIC ARTIST (STEP 10)  
+   }else if (char == 't'){
+    //OUTPUT TOTAL TIME OF PLAYLIST (STEP 11)
+   }else if (char == 'o'){
+    //OUTPUT FULL PLAYLIST (STEP 6)
+   }else if (char == 'q'){
+    //QUIT (i think this should just be a break statement?)
+   }
+   
+}//end of ExecuteMenu
+
+int main() {
+   //STEP 2 (complete)
+   cout << " Enter playlist's title: :" << endl;
+   cin >> playlistTitle;
+   
+   return 0;
+}
+
